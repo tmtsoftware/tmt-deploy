3b088d429f69d37b1289a7332eabbb749545dbdb
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-10-10 15:35:28 +0530

    Move script files into one directory
    Co-authored-by: Mushtaq Ahmed <5066+mushtaq@users.noreply.github.com>

--- scripts/csw-bootstrap.sh
+++ scripts/csw-bootstrap.sh
@@ -9,7 +9,7 @@ mkdir -p target/csw/"$1"/bin
 ./coursier bootstrap -r jitpack com.github.tmtsoftware.csw:csw-config-server_2.12:"$1" -M csw.config.server.Main -o target/csw/"$1"/bin/csw-config-server
 
 cp -r ./conf ./target/csw/"$1"
-cp ./csw-services.sh ./target/csw/"$1"/bin
+cp ./scripts/csw-services.sh ./target/csw/"$1"/bin
 cp ./scripts/redis-sentinel-prod.sh ./target/csw/"$1"/bin
 else
 echo "[ERROR] Please provide version ID as argument for creating the artifacts"
