b29bc8e3f1e3ba32a9d25a5382abba560f0c1de5
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-10-15 11:15:53 +0530

    esw-shell
    Try packing the app for esw-shell.

--- scripts/esw-bootstrap.sh
+++ scripts/esw-bootstrap.sh
@@ -4,7 +4,8 @@ if [ "$#" -ne 0 ]
 then
 mkdir -p target/esw/"$1"/bin
 
-./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype:ocs-gateway_2.12:"$1" -M ocs.gateway.GatewayApp -o target/esw/"$1"/bin/ocs-gateway
+./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype:ocs-gateway_2.12:"$1" -M ocs.gateway.GatewayApp -f -o target/esw/"$1"/bin/ocs-gateway
+./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype:ocs-framework_2.12:"$1" -M ocs.framework.TestAmmoniteApp -f -o target/esw/"$1"/bin/ocs-shell
 
 echo "Artifacts successfully generated"
 else
