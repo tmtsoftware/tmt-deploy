5a8d2cfed13e1ae0d4a839941597f9f67bf6a695
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-10-17 14:09:35 +0530

    Update

--- scripts/esw-bootstrap.sh
+++ scripts/esw-bootstrap.sh
@@ -5,7 +5,7 @@ then
 mkdir -p target/esw/"$1"/bin
 
 ./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype:ocs-gateway_2.12:"$1" -M ocs.gateway.GatewayApp -f -o target/esw/"$1"/bin/ocs-gateway
-./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype:ocs-framework_2.12:"$1" -M ocs.framework.TestAmmoniteApp -f -o target/esw/"$1"/bin/ocs-shell
+./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype:ocs-client_2.12:"$1" -M ocs.client.Main -f -o target/esw/"$1"/bin/ocs-client
 
 echo "Artifacts successfully generated"
 else
