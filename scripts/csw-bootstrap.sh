f4e2bc052ad2e15ae7466af7480f5694c3e83453
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-10-17 15:47:28 +0530

    use idiomatic artefact id

--- scripts/csw-bootstrap.sh
+++ scripts/csw-bootstrap.sh
@@ -4,9 +4,9 @@ if [ "$#" -ne 0 ]
 then
 mkdir -p target/csw/"$1"/bin
 
-./coursier bootstrap -r jitpack com.github.tmtsoftware.csw:csw-location-server_2.12:"$1" -M csw.location.server.Main -o target/csw/"$1"/bin/csw-location-server
-./coursier bootstrap -r jitpack com.github.tmtsoftware.csw:csw-location-agent_2.12:"$1" -M csw.location.agent.Main -o target/csw/"$1"/bin/csw-location-agent
-./coursier bootstrap -r jitpack com.github.tmtsoftware.csw:csw-config-server_2.12:"$1" -M csw.config.server.Main -o target/csw/"$1"/bin/csw-config-server
+./coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-location-server:"$1" -M csw.location.server.Main -o target/csw/"$1"/bin/csw-location-server
+./coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-location-agent:"$1" -M csw.location.agent.Main -o target/csw/"$1"/bin/csw-location-agent
+./coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-config-server:"$1" -M csw.config.server.Main -o target/csw/"$1"/bin/csw-config-server
 
 cp -r ./conf ./target/csw/"$1"
 cp ./scripts/csw-services.sh ./target/csw/"$1"/bin
--- scripts/esw-bootstrap.sh
+++ scripts/esw-bootstrap.sh
@@ -4,8 +4,8 @@ if [ "$#" -ne 0 ]
 then
 mkdir -p target/esw/"$1"/bin
 
-./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype:ocs-gateway_2.12:"$1" -M ocs.gateway.GatewayApp -f -o target/esw/"$1"/bin/ocs-gateway
-./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype:ocs-client_2.12:"$1" -M ocs.client.Main -f -o target/esw/"$1"/bin/ocs-client
+./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype::ocs-gateway:"$1" -M ocs.gateway.GatewayApp -f -o target/esw/"$1"/bin/ocs-gateway
+./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype::ocs-client:"$1" -M ocs.client.Main -f -o target/esw/"$1"/bin/ocs-client
 
 echo "Artifacts successfully generated"
 else
