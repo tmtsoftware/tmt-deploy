9de8e5025903dac5d1c4c8dede72d896dfbf3461
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-10-22 14:03:43 +0530

    Generate executable script for ocs-client using coursier launch

--- scripts/esw-bootstrap.sh
+++ scripts/esw-bootstrap.sh
@@ -5,7 +5,11 @@ then
 mkdir -p target/esw/"$1"/bin
 
 ./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype::ocs-gateway:"$1" -M ocs.gateway.GatewayApp -f -o target/esw/"$1"/bin/ocs-gateway
-./coursier bootstrap -r jitpack com.github.tmtsoftware.esw-prototype::ocs-client:"$1" -M ocs.client.Main -f -o target/esw/"$1"/bin/ocs-client
+cat > target/esw/"$1"/bin/ocs-client << EOL
+#!/usr/bin/env bash
+./coursier launch -r jitpack com.github.tmtsoftware.esw-prototype::ocs-client:$1 -M ocs.client.Main
+EOL
+chmod u+x target/esw/"$1"/bin/ocs-client
 
 echo "Artifacts successfully generated"
 else
